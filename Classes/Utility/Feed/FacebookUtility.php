23a8f85c4a4ab0219f1ff7a05316d4b32d170857
Daniel Diesenreither <d.diesenreither@siwa.at>
2018-08-30 17:51:59 +0200

    fixed stuff

--- Classes/Utility/Feed/FacebookUtility.php
+++ Classes/Utility/Feed/FacebookUtility.php
@@ -1,32 +1,33 @@
 <?php
+
 namespace Socialstream\SocialStream\Utility\Feed;
 
 
-    /***************************************************************
-     *
-     *  Copyright notice
-     *
-     *  (c) 2016
-     *
-     *  All rights reserved
-     *
-     *  This script is part of the TYPO3 project. The TYPO3 project is
-     *  free software; you can redistribute it and/or modify
-     *  it under the terms of the GNU General Public License as published by
-     *  the Free Software Foundation; either version 3 of the License, or
-     *  (at your option) any later version.
-     *
-     *  The GNU General Public License can be found at
-     *  http://www.gnu.org/copyleft/gpl.html.
-     *
-     *  This script is distributed in the hope that it will be useful,
-     *  but WITHOUT ANY WARRANTY; without even the implied warranty of
-     *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-     *  GNU General Public License for more details.
-     *
-     *  This copyright notice MUST APPEAR in all copies of the script!
-     ***************************************************************/
-    
+/***************************************************************
+ *
+ *  Copyright notice
+ *
+ *  (c) 2016
+ *
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
 use \TYPO3\CMS\Extbase\Utility\LocalizationUtility;
 use \TYPO3\CMS\Core\Messaging\AbstractMessage;
 use \TYPO3\CMS\Core\Messaging\FlashMessageService;
@@ -38,11 +39,12 @@ use \TYPO3\CMS\Core\Utility\GeneralUtility;
  */
 class FacebookUtility extends \Socialstream\SocialStream\Utility\Feed\FeedUtility
 {
-        
-    public function getChannel(\Socialstream\SocialStream\Domain\Model\Channel $channel,$isProcessing=0)
+
+    public function getChannel(\Socialstream\SocialStream\Domain\Model\Channel $channel, $isProcessing = 0)
     {
-        $url = "https://graph.facebook.com/".$channel->getObjectId()."/?fields=id,name,about,link,picture,cover&access_token=".$channel->getToken();
-        if($this->get_http_response_code($url) == 200) {
+        $url = "https://graph.facebook.com/" . $channel->getObjectId() . "/?fields=id,name,about,link,picture,cover&access_token=" . $channel->getToken();
+
+        if ($this->get_http_response_code($url) == 200) {
             $elem = $this->getElems($url);
 
             $channel->setObjectId($elem->id);
@@ -58,28 +60,29 @@ class FacebookUtility extends \Socialstream\SocialStream\Utility\Feed\FeedUtilit
                     $this->processChannelMedia($channel, $imageUrl);
                 }
             }
-        }else{
+        } else {
             if ($isProcessing == 0) {
-                if($this->settings["sysmail"]) {
-                    $this->sendTokenInfoMail($channel,$this->settings["sysmail"],$this->settings["sendermail"]);
+                if ($this->settings["sysmail"]) {
+                    $this->sendTokenInfoMail($channel, $this->settings["sysmail"], $this->settings["sendermail"]);
                 }
-            }else{
+            } else {
                 $msg = "Fehler: Channel konnte nicht gecrawlt werden. Object Id oder Token falsch.";
                 //$this->addFlashMessage($msg, '', AbstractMessage::ERROR);
                 $this->objectManager = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\\CMS\\Extbase\\Object\\ObjectManager');
-                $this->addFlashMessage($msg, '', FlashMessage::ERROR,$this->objectManager->get(FlashMessageService::class));                
+                $this->addFlashMessage($msg, '', FlashMessage::ERROR, $this->objectManager->get(FlashMessageService::class));
                 return false;
             }
         }
-                
+
         return $channel;
     }
 
-    public function renewToken(\Socialstream\SocialStream\Domain\Model\Channel $channel){
-        $expdiff = ($channel->getExpires() - time())/86400;
-        if($expdiff <= 5){
+    public function renewToken(\Socialstream\SocialStream\Domain\Model\Channel $channel)
+    {
+        $expdiff = ($channel->getExpires() - time()) / 86400;
+        if ($expdiff <= 5) {
             $url = "https://graph.facebook.com/oauth/access_token?grant_type=fb_exchange_token&client_id=" . $this->settings["fbappid"] . "&client_secret=" . $this->settings["fbappsecret"] . "&fb_exchange_token=" . $channel->getToken();
-            if($this->get_http_response_code($url) == 200) {
+            if ($this->get_http_response_code($url) == 200) {
                 $token = file_get_contents($url);
                 /*$infos = explode("&", $token);
                 $tk = explode("=", $infos[0])[1];
@@ -87,30 +90,31 @@ class FacebookUtility extends \Socialstream\SocialStream\Utility\Feed\FeedUtilit
                 $tk = json_decode($token);
                 $channel->setToken($tk->access_token);
                 $channel->setExpires(time() + $tk->expires_in);
-            }else{
-                if($this->settings["sysmail"]) {
-                    $this->sendTokenInfoMail($channel,$this->settings["sysmail"],$this->settings["sendermail"]);
+            } else {
+                if ($this->settings["sysmail"]) {
+                    $this->sendTokenInfoMail($channel, $this->settings["sysmail"], $this->settings["sendermail"]);
                 }
             }
         }
         return $channel;
     }
 
-    public function getFeed(\Socialstream\SocialStream\Domain\Model\Channel $channel,$limit=100){
+    public function getFeed(\Socialstream\SocialStream\Domain\Model\Channel $channel, $limit = 100)
+    {
         $this->persistenceManager = GeneralUtility::makeInstance('TYPO3\\CMS\\Extbase\\Persistence\\Generic\\PersistenceManager');
         $this->newsRepository = GeneralUtility::makeInstance('Socialstream\\SocialStream\\Domain\\Repository\\NewsRepository');
         $this->categoryRepository = GeneralUtility::makeInstance('GeorgRinger\\News\\Domain\\Repository\\CategoryRepository');
 
-        if($channel->getPosttype() == "1"){
-            $url = "https://graph.facebook.com/".$channel->getObjectId()."/posts?fields=id,created_time,link,permalink_url,place,type,message,full_picture,object_id,picture,name,caption,description,story,source,from&access_token=".$channel->getToken()."&limit=".$limit;
-        }else{
-            $url = "https://graph.facebook.com/".$channel->getObjectId()."/feed?fields=id,created_time,link,permalink_url,place,type,message,full_picture,object_id,picture,name,caption,description,story,source,from&access_token=".$channel->getToken()."&limit=".$limit;
+        if ($channel->getPosttype() == "1") {
+            $url = "https://graph.facebook.com/" . $channel->getObjectId() . "/posts?fields=id,created_time,link,permalink_url,place,type,message,full_picture,object_id,picture,name,caption,description,story,source,from&access_token=" . $channel->getToken() . "&limit=" . $limit;
+        } else {
+            $url = "https://graph.facebook.com/" . $channel->getObjectId() . "/feed?fields=id,created_time,link,permalink_url,place,type,message,full_picture,object_id,picture,name,caption,description,story,source,from&access_token=" . $channel->getToken() . "&limit=" . $limit;
         }
 
         $elem = $this->getElems($url);
-        
+
         foreach ($elem->data as $entry) {
-            if($entry->name || $entry->message) {
+            if ($entry->name || $entry->message) {
                 $new = 0;
                 $news = $this->newsRepository->findHiddenById($entry->id, $channel->getUid());
                 if (!$news) {
@@ -124,18 +128,18 @@ class FacebookUtility extends \Socialstream\SocialStream\Utility\Feed\FeedUtilit
                 $news->addCategory($cat);
                 $subcat = $this->getCategory($channel->getTitle(), $cat);
                 $news->addCategory($subcat);
-                $id = explode("_",$entry->id);
-                if($id[1]){
+                $id = explode("_", $entry->id);
+                if ($id[1]) {
                     $news->setObjectId($id[1]);
-                }else{
+                } else {
                     $news->setObjectId($entry->id);
-                }                
+                }
                 $news->setDatetime(new \DateTime($entry->created_time));
                 if ($entry->link) $news->setLink($entry->link);
                 $news->setAuthor($entry->from->name);
-                if ($entry->name){
+                if ($entry->name) {
                     $news->setTitle($entry->name);
-                }else{
+                } else {
                     $news->setTitle($entry->from->name);
                 }
                 if ($entry->place) {
@@ -190,13 +194,15 @@ class FacebookUtility extends \Socialstream\SocialStream\Utility\Feed\FeedUtilit
                     $imageUrl = $singlePost->full_picture;
                 }
 
+
                 $media = $this->validateMedia($channel, $imageUrl, $videoUrl);
 
-                if(is_array($media)){
-                    if($media['link']){
+
+                if (is_array($media)) {
+                    if ($media['link']) {
                         $news->setMediaUrl($media['link']);
                     }
-                    if($media['media_url']){
+                    if ($media['media_url']) {
                         $this->processNewsMedia($news, $media['media_url']);
                     }
                 }
--- Classes/Utility/Feed/FeedUtility.php
+++ Classes/Utility/Feed/FeedUtility.php
@@ -193,6 +193,54 @@ class FeedUtility extends \Socialstream\SocialStream\Utility\BaseUtility
         $this->processMedia($news, "tx_news_domain_model_news", "fal_media", $folder, $imageUrl);
     }
 
+    function getExtensionFromMimeType($mime)
+    {
+        $all_mimes = '{"png":["image\/png","image\/x-png"],"bmp":["image\/bmp","image\/x-bmp","image\/x-bitmap","image\/x-xbitmap","image\/x-win-bitmap","image\/x-windows-bmp","image\/ms-bmp","image\/x-ms-bmp","application\/bmp","application\/x-bmp","application\/x-win-bitmap"],"gif":["image\/gif"],"jpeg":["image\/jpeg","image\/pjpeg"],"xspf":["application\/xspf+xml"],"vlc":["application\/videolan"],"wmv":["video\/x-ms-wmv","video\/x-ms-asf"],"au":["audio\/x-au"],"ac3":["audio\/ac3"],"flac":["audio\/x-flac"],"ogg":["audio\/ogg","video\/ogg","application\/ogg"],"kmz":["application\/vnd.google-earth.kmz"],"kml":["application\/vnd.google-earth.kml+xml"],"rtx":["text\/richtext"],"rtf":["text\/rtf"],"jar":["application\/java-archive","application\/x-java-application","application\/x-jar"],"zip":["application\/x-zip","application\/zip","application\/x-zip-compressed","application\/s-compressed","multipart\/x-zip"],"7zip":["application\/x-compressed"],"xml":["application\/xml","text\/xml"],"svg":["image\/svg+xml"],"3g2":["video\/3gpp2"],"3gp":["video\/3gp","video\/3gpp"],"mp4":["video\/mp4"],"m4a":["audio\/x-m4a"],"f4v":["video\/x-f4v"],"flv":["video\/x-flv"],"webm":["video\/webm"],"aac":["audio\/x-acc"],"m4u":["application\/vnd.mpegurl"],"pdf":["application\/pdf","application\/octet-stream"],"pptx":["application\/vnd.openxmlformats-officedocument.presentationml.presentation"],"ppt":["application\/powerpoint","application\/vnd.ms-powerpoint","application\/vnd.ms-office","application\/msword"],"docx":["application\/vnd.openxmlformats-officedocument.wordprocessingml.document"],"xlsx":["application\/vnd.openxmlformats-officedocument.spreadsheetml.sheet","application\/vnd.ms-excel"],"xl":["application\/excel"],"xls":["application\/msexcel","application\/x-msexcel","application\/x-ms-excel","application\/x-excel","application\/x-dos_ms_excel","application\/xls","application\/x-xls"],"xsl":["text\/xsl"],"mpeg":["video\/mpeg"],"mov":["video\/quicktime"],"avi":["video\/x-msvideo","video\/msvideo","video\/avi","application\/x-troff-msvideo"],"movie":["video\/x-sgi-movie"],"log":["text\/x-log"],"txt":["text\/plain"],"css":["text\/css"],"html":["text\/html"],"wav":["audio\/x-wav","audio\/wave","audio\/wav"],"xhtml":["application\/xhtml+xml"],"tar":["application\/x-tar"],"tgz":["application\/x-gzip-compressed"],"psd":["application\/x-photoshop","image\/vnd.adobe.photoshop"],"exe":["application\/x-msdownload"],"js":["application\/x-javascript"],"mp3":["audio\/mpeg","audio\/mpg","audio\/mpeg3","audio\/mp3"],"rar":["application\/x-rar","application\/rar","application\/x-rar-compressed"],"gzip":["application\/x-gzip"],"hqx":["application\/mac-binhex40","application\/mac-binhex","application\/x-binhex40","application\/x-mac-binhex40"],"cpt":["application\/mac-compactpro"],"bin":["application\/macbinary","application\/mac-binary","application\/x-binary","application\/x-macbinary"],"oda":["application\/oda"],"ai":["application\/postscript"],"smil":["application\/smil"],"mif":["application\/vnd.mif"],"wbxml":["application\/wbxml"],"wmlc":["application\/wmlc"],"dcr":["application\/x-director"],"dvi":["application\/x-dvi"],"gtar":["application\/x-gtar"],"php":["application\/x-httpd-php","application\/php","application\/x-php","text\/php","text\/x-php","application\/x-httpd-php-source"],"swf":["application\/x-shockwave-flash"],"sit":["application\/x-stuffit"],"z":["application\/x-compress"],"mid":["audio\/midi"],"aif":["audio\/x-aiff","audio\/aiff"],"ram":["audio\/x-pn-realaudio"],"rpm":["audio\/x-pn-realaudio-plugin"],"ra":["audio\/x-realaudio"],"rv":["video\/vnd.rn-realvideo"],"jp2":["image\/jp2","video\/mj2","image\/jpx","image\/jpm"],"tiff":["image\/tiff"],"eml":["message\/rfc822"],"pem":["application\/x-x509-user-cert","application\/x-pem-file"],"p10":["application\/x-pkcs10","application\/pkcs10"],"p12":["application\/x-pkcs12"],"p7a":["application\/x-pkcs7-signature"],"p7c":["application\/pkcs7-mime","application\/x-pkcs7-mime"],"p7r":["application\/x-pkcs7-certreqresp"],"p7s":["application\/pkcs7-signature"],"crt":["application\/x-x509-ca-cert","application\/pkix-cert"],"crl":["application\/pkix-crl","application\/pkcs-crl"],"pgp":["application\/pgp"],"gpg":["application\/gpg-keys"],"rsa":["application\/x-pkcs7"],"ics":["text\/calendar"],"zsh":["text\/x-scriptzsh"],"cdr":["application\/cdr","application\/coreldraw","application\/x-cdr","application\/x-coreldraw","image\/cdr","image\/x-cdr","zz-application\/zz-winassoc-cdr"],"wma":["audio\/x-ms-wma"],"vcf":["text\/x-vcard"],"srt":["text\/srt"],"vtt":["text\/vtt"],"ico":["image\/x-icon","image\/x-ico","image\/vnd.microsoft.icon"],"csv":["text\/x-comma-separated-values","text\/comma-separated-values","application\/vnd.msexcel"],"json":["application\/json","text\/json"]}';
+        $all_mimes = json_decode($all_mimes, true);
+        foreach ($all_mimes as $key => $value) {
+            if (array_search($mime, $value) !== false) return $key;
+        }
+        return false;
+    }
+
+    /**
+     * @param string $url
+     * @param $model
+     * @return array
+     */
+    function grabImage($url, $model)
+    {
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => $url,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => "",
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 30,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => "GET",
+            CURLOPT_HTTPHEADER => array(
+                "Cache-Control: no-cache"
+            ),
+        ));
+
+        $response = curl_exec($curl);
+        $err = curl_error($curl);
+
+        if ($err) {
+            echo "cURL Error #:" . $err;
+        } else {
+            $basename = str_replace('safe_image.php', '', $url);
+            $imageName = $model->getObjectId() . "_" . explode('.', basename($basename))[0] . "." . $this->getExtensionFromMimeType(curl_getinfo($curl, CURLINFO_CONTENT_TYPE));
+            return array(
+                'imageName' => $imageName,
+                'image' => $response
+            );
+        }
+        curl_close($curl);
+    }
+
     /**
      * @param $model
      * @param $table
@@ -202,20 +250,20 @@ class FeedUtility extends \Socialstream\SocialStream\Utility\BaseUtility
      */
     protected function processMedia($model, $table, $field, $folder, $imageUrl)
     {
-        $imageName = explode("?", basename($imageUrl));
-        if (is_array($imageName)) {
-            if (strpos($imageName[0], 'safe_image.php') !== false) {
-                $imageName = basename(urldecode($imageName[1]));
-            } else {
-                $imageName = $imageName[0];
-            }
-        }
-        //$imageName = $model->getObjectId().".jpg";
+        $imageArray = $this->grabImage($imageUrl, $model);
+
+        $imageName = $imageArray['imageName'];
 
         $storage = $this->getStorage();
 
         if ((!$folder->hasFile($imageName) && $imageName) || ($storage->getFileInFolder($imageName, $folder)->getSize() <= 0 && $folder->hasFile($imageName) && $imageName)) {
-            $this->grab_image($imageUrl, $this->settings["tmp"] . $imageName);
+            if (file_exists($this->settings["tmp"] . $imageName)) {
+                unlink($this->settings["tmp"] . $imageName);
+            }
+            $fp = fopen($this->settings["tmp"] . $imageName, 'x');
+            fwrite($fp, $imageArray['image']);
+            fclose($fp);
+
             if (filesize($this->settings["tmp"] . $imageName) > 0) {
                 $movedNewFile = $storage->addFile($this->settings["tmp"] . $imageName, $folder, $imageName, \TYPO3\CMS\Core\Resource\DuplicationBehavior::REPLACE);
                 $image = $movedNewFile->getUid();
@@ -283,16 +331,6 @@ class FeedUtility extends \Socialstream\SocialStream\Utility\BaseUtility
      */
     protected function grab_image($url, $saveto)
     {
-        if (strpos($url, 'safe_image.php') !== false && strpos($url, '&url=') !== false && strpos($url, '&url=fbstaging') === false) {
-            $urlpath = preg_replace_callback('/\\\\u([0-9a-fA-F]{4})/', function ($match) {
-                return mb_convert_encoding(pack('H*', $match[1]), 'UTF-8', 'UCS-2BE');
-            }, $url);
-
-            $parts = parse_url($urlpath);
-            parse_str($parts['query'], $query);
-            $url = $query['url'];
-        }
-
         $ch = curl_init($url);
         curl_setopt($ch, CURLOPT_HEADER, 0);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
