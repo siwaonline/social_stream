e6440bc8c41460010837d7d7806068cdbdec187b
siwa_pradler <p.radler@siwa.at>
2022-06-08 10:11:43 +0200

    V11 compatibility ext_emconf

--- ext_emconf.php
+++ ext_emconf.php
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'version' => '4.0.0',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '10.4.0-10.4.99',
+			'typo3' => '11.5.0-11.5.99',
 			'news' => '^8.5.0',
 		),
 		'conflicts' => array(
